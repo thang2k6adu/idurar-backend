37e648e8b37cd2fdec6edbf9291737a99b9c851c
thang2k6adu <thang2k6adu@gmail.com>
2025-06-23 17:21:18 +0700

    Changes: - Renamed the models files to Admin.js and AdminPassword.js - Modified and added several additional fields to complete the model - Refactored Controllers - Write the token validation controller middleware function (isValidAuthToken)

---
+++ src/models/coreModels/AdminPassword.js
@@ -0,0 +1,57 @@
+import mongoose from 'mongoose'
+import bcrypt from 'bcrypt'
+const Schema = mongoose.Schema
+
+const AdminPasswordSchema = new Schema({
+  removed: {
+    type: Boolean,
+    default: false,
+  },
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'Admin',
+    required: true,
+    unique: true,
+  },
+  emailToken: {
+    type: String,
+  },
+  resetToken: {
+    type: String,
+  },
+  emailVerified: {
+    type: Boolean,
+    default: false,
+  },
+  authType: {
+    type: String,
+    default: 'email',
+  },
+  loggedSessions: {
+    type: [String],
+    default: [],
+  },
+})
+
+AdminPasswordSchema.methods.hashPassword = async (password) => {
+  return await bcrypt.hash(password, 10)
+}
+
+AdminPasswordSchema.methods.isValidPassword = async (password) => {
+  return await bcrypt.compare(password, this.password)
+}
+// const userPasswordSchema = new mongoose.Schema(
+//   {
+//     // ObjectId is not a JS type, it's a mongoose type
+//     user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+//     password: { type: String, required: true },
+//     loggedSessions: [{ type: String }],
+//     emailVerified: { type: Boolean, default: false },
+//     emailToken: { type: String },
+//     resetToken: { type: String },
+//     removed: { type: Boolean, default: false },
+//   },
+//   { timestamps: true }
+// )
+
+export const AdminPassword = mongoose.model('AdminPassword', AdminPasswordSchema)
